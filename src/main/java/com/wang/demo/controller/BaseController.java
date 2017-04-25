90d5e53c222399e28501599cb963fd4f2e80dadc
wangjianxiongwjx <337496746@qq.com>
2017-04-25 16:47:12 +0800

    导入

--- src/main/java/com/wang/demo/controller/BaseController.java
+++ src/main/java/com/wang/demo/controller/BaseController.java
@@ -1,5 +1,23 @@
 package com.wang.demo.controller;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
 public class BaseController {
+	
+	@RequestMapping("/hello")
+    @ResponseBody
+    String home() {
+        return "Hello ,spring boot!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(BaseController.class, args);
+    }
 
 }
